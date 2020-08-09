3ca7d798a308edd477155584486fe9f0ac04b556
lonetargaryen5 <swarnim.sonawane@gmail.com>
2020-08-09 15:53:49 +0530

    Initial commit.

---
+++ src/leavemanagement/App.java
@@ -0,0 +1,7 @@
+package leavemanagement;
+
+public class App {
+    public static void main(String[] args) {
+        Menu.displayMainMenu();;
+    }
+}
---
+++ src/leavemanagement/Menu.java
@@ -0,0 +1,10 @@
+package leavemanagement;
+
+public class Menu {
+    static void displayMainMenu() {
+        System.out.println("\n-------------- LEAVE MANAGEMENT SYSTEM --------------\n");
+        System.out.println("1. Login.\n");
+        System.out.println("2. Register.\n");
+        System.out.println("Enter your choice: ");
+    }
+}
